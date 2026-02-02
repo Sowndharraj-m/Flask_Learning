eca2046034e72780d961f2db590daab5735a6c70
sowndhar-m <sowndharrajm17@gmail.com>
2026-02-02 17:16:47 +0530

    1st_flask_project

--- bs.py
+++ bs.py
@@ -1,10 +1,39 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , redirect ,request ,url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+todos = [{"task" :"sample todo 1" , "done": False}]
+       
 
 @app.route('/')
 def home():
-       return render_template('index.html')
+       return render_template('index.html', todos=todos)
+
+@app.route('/add' , methods = ['POST'])
+def add():
+       todo_task = request.form.get('todo_task')
+       todos.append({"task": todo_task , "done": False})
+       return redirect(url_for('home'))
+
+@app.route('/edit/<int:home>' , methods = ['GET' , 'POST'])
+def edit(home):
+       todo_task = todos[home]
+       if request.method == 'POST':
+              todo_task['task'] = request.form.get('todo_task')
+              return redirect(url_for('home'))
+       else:
+              return render_template('edit.html' , todo=todo_task, home=home)
+       
+@app.route('/check/<int:home>')
+def check(home):
+       todos[home]['done'] = not todos[home]['done']
+       return redirect(url_for('home'))
+
+@app.route('/delete/<int:home>')
+def delete(home):
+       todos.pop(home)
+       return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
        app.run(debug=True)
---
+++ cmds.md
@@ -0,0 +1,3 @@
+ - `py -m venv env` - to create a virtual environment named `env`
+ - `env/Scripts/activate` (Windows) or `source env/bin/activate` (Mac/Linux)
+ 
